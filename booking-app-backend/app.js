0a88fe18247fa48cbfea8999dae115ccf34dcbc5
isHan-8 <0808cs211078.ies@ipsacademy.org>
2024-09-20 10:20:27 +0530

    first commit

--- booking-app-backend/app.js
+++ booking-app-backend/app.js
@@ -11,7 +11,9 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(bodyParser.json());
-
+app.use("/test", (req, res) => {
+  res.send("Hello world!");
+});
 // Connect to MongoDB
 mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
